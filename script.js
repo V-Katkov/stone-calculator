b304c5d4766108bed63d83f1ddad4e1e3f1d5824
Vlad <v.katk0ov@gmail.com>
2024-05-09 00:14:00 +0300

    v1.1

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
 const submitBtn = document.getElementById('submitBtn');
 
-submitBtn.addEventListener('click', () => {
-    const stoneWidth = document.getElementById('stoneWidth').value;
-    const stoneHeight = document.getElementById('stoneHeight').value;
-    const stoneWeight = document.getElementById('stoneWeight').value;
-    const resultFace = document.getElementById('resultFace');
-    const face = (stoneWidth / 100) * (stoneHeight / 100);
-    const top = (stoneWidth / 100) * (stoneWeight / 100);
-    const left = (stoneHeight / 100) * (stoneWeight / 100);
+window.addEventListener('click', () => {
+    const get = {
+        width: document.getElementById('getWidth').value,
+        height: document.getElementById('getHeight').value,
+        weight: document.getElementById('getWeight').value
+    }
+    const stoneFace = ((get.width / 100) * (get.height / 100)).toFixed(3);
+    const stoneTop = ((get.width / 100) * (get.weight / 100)).toFixed(3);
+    const stoneSide = ((get.height / 100) * (get.weight / 100)).toFixed(3);
+
+    const result = {
+        face1: document.getElementById('resultFace1'),
+        face2: document.getElementById('resultFace2'),
+        top: document.getElementById('resultTop'),
+        bottom: document.getElementById('resultBottom'),
+        left: document.getElementById('resultLeft'),
+        right: document.getElementById('resultRight'),
+        general: document.getElementById('result')
+    }
+    const check = {
+        face1: document.getElementById('checkFace1'),
+        face2: document.getElementById('checkFace2'),
+        top: document.getElementById('checkTop'),
+        bottom: document.getElementById('checkBottom'),
+        left: document.getElementById('checkLeft'),
+        right: document.getElementById('checkRight'),
+    }
+
+    const generalResult = () => {
+        let summ = 0;
+        if (check.face1.checked) {
+            summ = summ + +stoneFace;
+        }
+        if (check.face2.checked) {
+            summ = summ + +stoneFace;
+        }
+        if (check.top.checked) {
+            summ = summ + +stoneTop;
+        }
+        if (check.bottom.checked) {
+            summ = summ + +stoneTop;
+        }
+        if (check.left.checked) {
+            summ = summ + +stoneSide;
+        }
+        if (check.right.checked) {
+            summ = summ + +stoneSide;
+        }
+        return summ.toFixed(3);
+    }
 
 
-
-    resultFace.innerHTML = `<span>Лице 1: ${stoneWidth}см. х ${stoneHeight}см.</span><b>${face}кв.<b>`;
-    resultFace2.innerHTML = `<span>Лице 2: ${stoneWidth}см. х ${stoneHeight}см.</span> <b>${face}кв.<b>`;
-    resultTop.innerHTML = `<span>Лобік верхній: ${stoneWidth}см. х ${stoneWeight}см.</span> <b>${top}кв.<b>`;
-    resultLeft.innerHTML = `<span>Лобік бік 1: ${stoneHeight}см. х ${stoneWeight}см.</span> <b>${left}кв.<b>`;
-    resultRight.innerHTML = `<span>Лобік бік 2: ${stoneHeight}см. х ${stoneWeight}см.</span> <b>${left}кв.<b>`;
-    result.innerHTML = `<b>${(face*2) + top + (left*2)}кв.</b>`
+    result.face1.innerHTML = `<span>Лице 1: ${get.width}см. х ${get.height}см.</span> <b>${stoneFace}кв.</b>`;
+    result.face2.innerHTML = `<span>Лице 2: ${get.width}см. х ${get.height}см.</span> <b>${stoneFace}кв.</b>`;
+    result.left.innerHTML = `<span>Лобік бік 1: ${get.height}см. х ${get.weight}см.</span> <b>${stoneSide}кв.</b>`;
+    result.right.innerHTML = `<span>Лобік бік 2: ${get.height}см. х ${get.weight}см.</span> <b>${stoneSide}кв.</b>`;
+    result.top.innerHTML = `<span>Лобік верхній: ${get.width}см. х ${get.weight}см.</span> <b>${stoneTop}кв.</b>`;
+    result.bottom.innerHTML = `<span>Лобік нижній: ${get.width}см. х ${get.weight}см.</span> <b>${stoneTop}кв.</b>`;
+    result.general.innerHTML = `<b>${generalResult()}кв.</b>`
 })
